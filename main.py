6827fe29c61f9a73196147f74c846878599c093b
susan <susan.belling@peachpayments.com>
2022-12-29 09:24:04 +0200

    Improvements to functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -36,15 +37,56 @@ def save():
     password = password_input.get()
 
     if not website or not password:
-        messagebox.showwarning(title="Missing Data", message="Please double check the form.\n\nSome fields have been left empty")
+        messagebox.showwarning(
+            title="Missing Data",
+            message="Please double check the form.\n\nSome fields have been left empty"
+        )
     else:
-        can_save = messagebox.askokcancel(title=website, message=f"{website}\n\nWould you like to save your password as '{password}'?")
+        can_save = messagebox.askokcancel(
+            title=website,
+            message=f"{website.upper()}\n\nWould you like to save your password as '{password}'?"
+        )
 
         if can_save:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {password}\n")
+            new_data = {
+                website.upper(): {
+                    "password": password
+                }
+            }
+            try:
+                with open("data.json", "r") as data_file:
+                    data = json.load(data_file)
+                    data.update(new_data)
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            finally:
                 website_input.delete(0, END)
                 password_input.delete(0, END)
+
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+
+def search():
+    website = website_input.get().upper()
+
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+        if website in data:
+            password = data[website]["password"]
+            messagebox.showinfo(
+                title=f"{website} Found",
+                message=f"Password: {password}\n\nCopied successfully"
+            )
+            pyperclip.copy(password)
+    except FileNotFoundError:
+        messagebox.showwarning(
+            title=f"{website} Not Found",
+            message=f"The website {website} was not found"
+        )
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -61,12 +103,16 @@ canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website", width=10)
 website_label.grid(column=0, row=1)
-website_input = Entry(width=35)
-website_input.grid(column=1, row=1, columnspan=2)
+website_input = Entry(width=20)
+website_input.grid(column=1, row=1)
 website_input.focus()
+
+search_btn = Button(text="Search", width=11, command=search)
+search_btn.grid(column=2, row=1)
 
 password_label = Label(text="Password", width=10)
 password_label.grid(column=0, row=3)
+
 password_input = Entry(width=20)
 password_input.grid(column=1, row=3)
 
